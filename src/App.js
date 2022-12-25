f471c58bab91c9a9b78d0a4a6d802ed26c9be2ff
Ilya <lord-ilja@yandex.ru>
2022-12-25 19:15:49 +0300

    init commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import Game from './components/Game'
+import Result from './components/Result'
+
+import './index.scss'
+
+function App() {
+  return (
+    <div className="App">
+      <Game />
+      {/* <Result /> */}
+    </div>
+  )
+}
+
+export default App

1e0f9103e1944c83853988605bc05d2fefa586ac
Ilya <lord-ilja@yandex.ru>
2022-12-26 20:26:45 +0300

    add Result component

--- src/components/Result.jsx
+++ src/components/Result.jsx
@@ -1,9 +1,27 @@
-const Result = () => {
+const Result = ({ correct, quantity, onReloadGame }) => {
+  let text = ''
+  switch (correct) {
+    case 0:
+    case 5:
+      text = 'ответов'
+      break
+    case 1:
+      text = 'ответ'
+      break
+    default:
+      text = 'ответа'
+  }
+
   return (
     <div className="result">
-      <img src="https://cdn-icons-png.flaticon.com/512/2278/2278992.png" />
-      <h2>Вы отгадали 3 ответа из 10</h2>
-      <button>Попробовать снова</button>
+      <img
+        src="https://cdn-icons-png.flaticon.com/512/2278/2278992.png"
+        alt="finish"
+      />
+      <h2>
+        Вы отгадали {correct} {text} из {quantity}
+      </h2>
+      <button onClick={onReloadGame}>Попробовать снова</button>
     </div>
   )
 }
